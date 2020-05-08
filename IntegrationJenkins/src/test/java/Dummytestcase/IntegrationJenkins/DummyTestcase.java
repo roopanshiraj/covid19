e92fb91bc96160fc4d7b510fcf2f641366d77544
hp <hp@192.168.1.13>
2020-05-09 03:01:57 +0530

    first push

---
+++ IntegrationJenkins/src/test/java/Dummytestcase/IntegrationJenkins/DummyTestcase.java
@@ -0,0 +1,17 @@
+package Dummytestcase.IntegrationJenkins;
+
+import org.testng.annotations.Test;
+
+public class DummyTestcase {
+	
+		
+		@Test
+		public void testcase()
+		{
+			System.out.println(" I am a testcase");
+			System.out.println(" I am a testcase1");
+			System.out.println(" I am a testcase2");
+		}
+
+
+}
